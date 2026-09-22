fc59c10dd5548b5fe0b506c1063a6766c7d5adbc
sauravraj2003 <sr11122003@gmail.com>
2026-09-22 14:59:47 +0530

    chore: remove build helper scripts

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import ChatInterface from "./pages/ChatInterface";
+import CreateGame from "./pages/CreateGame";
+import GameWorkspace from "./pages/GameWorkspace";
+import AuthCallback from "./pages/AuthCallback";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/chat" element={<ChatInterface />} />
+          <Route path="/create-game" element={<CreateGame />} />
+          <Route path="/workspace" element={<GameWorkspace />} />
+          <Route path="/auth/callback" element={<AuthCallback />} />
+          {/* Catch-all */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
